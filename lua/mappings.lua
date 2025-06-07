99829cbd0bccff1c48f0e595dee164a1419cc925
MillerTKG <cjrdm.mt@gmail.com>
2025-06-07 14:47:46 -0400

    keymaps and comment plugin

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,161 @@
-require "nvchad.mappings"
-
--- add yours here
+-- require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+-- map("i", "<C-e>", "<End>", { desc = "move end of line" })
+
+-- map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
+-- map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
+map('n', '<leader>ch', '<cmd>NvCheatsheet<CR>', { desc = 'toggle nvcheatsheet' })
+
+map({ 'n', 'x' }, '<leader>fm', function()
+  require('conform').format { lsp_fallback = true }
+end, { desc = 'general format file' })
+
+-- global lsp mappings
+map('n', '<leader>ds', vim.diagnostic.setloclist, { desc = 'LSP diagnostic loclist' })
+
+-- tabufline
+map('n', '<leader>b', '<cmd>enew<CR>', { desc = 'buffer new' })
+
+map('n', '<tab>', function()
+  require('nvchad.tabufline').next()
+end, { desc = 'buffer goto next' })
+
+map('n', '<S-tab>', function()
+  require('nvchad.tabufline').prev()
+end, { desc = 'buffer goto prev' })
+
+map('n', '<leader>x', function()
+  require('nvchad.tabufline').close_buffer()
+end, { desc = 'buffer close' })
+
+-- -- Comment
+-- map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+-- map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
+-- nvimtree
+map('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', { desc = 'nvimtree toggle window' })
+map('n', '<leader>e', '<cmd>NvimTreeFocus<CR>', { desc = 'nvimtree focus window' })
+
+-- -- telescope
+-- map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+-- map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+-- map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+-- map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+-- map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+-- map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+-- map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+-- map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+-- map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
+
+map('n', '<leader>th', function()
+  require('nvchad.themes').open()
+end, { desc = 'telescope nvchad themes' })
+
+-- map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+-- map(
+--   "n",
+--   "<leader>fa",
+--   "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+--   { desc = "telescope find all files" }
+-- )
+
+-- terminal
+map('t', '<C-x>', '<C-\\><C-N>', { desc = 'terminal escape terminal mode' })
+
+-- new terminals
+map('n', '<leader>h', function()
+  require('nvchad.term').new { pos = 'sp' }
+end, { desc = 'terminal new horizontal term' })
+
+map('n', '<leader>v', function()
+  require('nvchad.term').new { pos = 'vsp' }
+end, { desc = 'terminal new vertical term' })
+--
+-- toggleable
+map({ 'n', 't' }, '<A-v>', function()
+  require('nvchad.term').toggle { pos = 'vsp', id = 'vtoggleTerm' }
+end, { desc = 'terminal toggleable vertical term' })
+
+map({ 'n', 't' }, '<A-h>', function()
+  require('nvchad.term').toggle { pos = 'sp', id = 'htoggleTerm' }
+end, { desc = 'terminal toggleable horizontal term' })
+
+map({ 'n', 't' }, '<A-i>', function()
+  require('nvchad.term').toggle { pos = 'float', id = 'floatTerm' }
+end, { desc = 'terminal toggle floating term' })
+
+-- whichkey
+map('n', '<leader>wK', '<cmd>WhichKey <CR>', { desc = 'whichkey all keymaps' })
+
+map('n', '<leader>wk', function()
+  vim.cmd('WhichKey ' .. vim.fn.input 'WhichKey: ')
+end, { desc = 'whichkey query lookup' })
+
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- save file
+map('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- save file without auto-formatting
+map('n', '<leader><C-s>n', '<cmd>noautocmd w <CR>', opts and { desc = 'Save with [N]o Formatting' })
+--
+-- quit file
+map('n', '<C-q>', '<cmd> q <CR>', opts)
+
+-- delete single character without copying into register
+map('n', 'x', '"_x', opts)
+
+-- Vertical scroll and center
+map('n', '<C-d>', '<C-d>zz', opts)
+map('n', '<C-u>', '<C-u>zz', opts)
+
+-- Find and center
+map('n', 'n', 'nzzzv', opts)
+map('n', 'N', 'Nzzzv', opts)
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+-- map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- map('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open Directory Listing' })
+
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- NOTE: Navigate between splits
+map('n', '<C-h>', ':wincmd h<CR>', opts)
+map('n', '<C-l>', ':wincmd l<CR>', opts)
+map('n', '<C-j>', ':wincmd j<CR>', opts)
+map('n', '<C-k>', ':wincmd k<CR>', opts)
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- vim: ts=2 sts=2 sw=2 et
