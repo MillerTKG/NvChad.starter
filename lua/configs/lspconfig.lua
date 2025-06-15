abd42a0e3a936f82f71b7c9320bcd2fe70ccd86d
MillerTKG <cjrdm.mt@gmail.com>
2025-06-15 10:58:10 -0400

    autohotkey lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,51 @@
 require('nvchad.configs.lspconfig').defaults()
 
-local servers = { 'html', 'cssls', 'autohotkey_lsp' }
+vim.lsp.config('autohotkey_lsp', {
+	autostart = true,
+	cmd = {
+		'node',
+		vim.fn.expand('$HOME/vscode-autohotkey2-lsp/server/dist/server.js'),
+		'--stdio',
+	},
+	filetypes = { 'ahk', 'autohotkey', 'ah2' },
+	init_options = {
+		ActionWhenV1IsDetected = 'Continue',
+		AutoLibInclude = 'All',
+		CommentTags = '^;;\\s*(?<tag>.+)',
+		CompleteFunctionParens = false,
+		Diagnostics = {
+			ClassStaticMemberCheck = true,
+			ParamsCheck = true,
+		},
+		FormatOptions = {
+			array_style = 'expand',
+			brace_style = 'One True Brace',
+			break_chained_methods = false,
+			ignore_comment = false,
+			indent_string = '\t',
+			max_preserve_newlines = 2,
+			object_style = 'none',
+			preserve_newlines = true,
+			space_after_double_colon = true,
+			space_before_conditional = true,
+			space_in_empty_paren = false,
+			space_in_other = true,
+			space_in_paren = false,
+			wrap_line_length = 0,
+		},
+		InterpreterPath = '',
+		SymbolFoldinFromOpenBrace = false,
+		locale = 'en-us',
+	},
+	single_file_support = true,
+	flags = { debounce_text_changes = 500 },
+	capabilities = require('nvchad.configs.lspconfig').capabilities,
+	on_attach = require('nvchad.configs.lspconfig').on_attach,
+})
+
+vim.lsp.enable('autohotkey_lsp')
+
+local servers = { 'html', 'cssls' }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
